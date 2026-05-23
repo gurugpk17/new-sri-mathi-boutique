f3eb5e5bd368be63ab7f1e6ffd4ddc402adaa9a5
gurugpk17 <gurumoorthy.2002@sritcbe.ac.in>
2026-05-24 01:22:00 +0530

    changed supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,16 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import ws from "ws";
 
-const supabaseUrl = "https://fvtesyqawdrjokfwjpyv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2dGVzeXFhd2Ryam9rZndqcHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NjE3NzgsImV4cCI6MjA5NTAzNzc3OH0.TvR92xnufqTTKF839IbVIeJF9sAuhMp0ziGmMPoeLPI";
-
+const supabaseUrl = process.env.VITE_SUPABASE_URL!;
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseKey,
-  {
-    realtime: {
-      transport: ws,
-    },
-  }
+  supabaseKey
 );
